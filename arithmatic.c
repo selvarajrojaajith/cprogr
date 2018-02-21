d3bc705bf3e816a436a6b2dd8dad7a5616b39535
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-21 16:47:21 +0530

    arithmatic.c

---
+++ arithmatic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+{
+int main()
+    int  i;
+    printf("Enter the number of terms");
+    scanf("%d", &terms);
+    printf("Enter first term of AP series");
+    scanf("%d %d", i);
+getch();
+}
+ 
